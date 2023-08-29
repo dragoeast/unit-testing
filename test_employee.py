73c41d6bb9e6d93eb11ab2295992eaed4d371550
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-08-29 11:11:53 -0500

    setUp for TestEmployee class added

--- test_employee.py
+++ test_employee.py
@@ -3,40 +3,38 @@ from employee import Employee
 
 
 class TestEmployee(unittest.TestCase):
+    def setUp(self) -> None:
+        self.employee_1 = Employee("Krisztian", "Markella", 60_000)
+        self.employee_2 = Employee("Christian", "Markella", 10_000)
+
+    def tearDown(self) -> None:
+        pass
+
     def test_email(self):
-        employee_1 = Employee("Krisztian", "Markella", 60_000)
-        employee_2 = Employee("Christian", "Markella", 10_000)
-
-        self.assertEqual(employee_1.email, "Krisztian.Markella@email.com")
-        self.assertEqual(employee_2.email, "Christian.Markella@email.com")
+        self.assertEqual(self.employee_1.email, "Krisztian.Markella@email.com")
+        self.assertEqual(self.employee_2.email, "Christian.Markella@email.com")
         
-        employee_2.first = "Mark"
+        self.employee_2.first = "Mark"
 
-        self.assertEqual(employee_2.email, "Mark.Markella@email.com")
+        self.assertEqual(self.employee_2.email, "Mark.Markella@email.com")
 
     def test_fullname(self):
-        employee_1 = Employee("Krisztian", "Markella", 60_000)
-        employee_2 = Employee("Christian", "Markella", 10_000)
-
-        self.assertEqual(employee_1.fullname, "Krisztian Markella")
-        self.assertEqual(employee_2.fullname, "Christian Markella")
+        self.assertEqual(self.employee_1.fullname, "Krisztian Markella")
+        self.assertEqual(self.employee_2.fullname, "Christian Markella")
         
-        employee_2.first = "Mark"
+        self.employee_2.first = "Mark"
 
-        self.assertEqual(employee_2.fullname, "Mark Markella")
+        self.assertEqual(self.employee_2.fullname, "Mark Markella")
 
     def test_raise(self):
-        employee_1 = Employee("Krisztian", "Markella", 60_000)
-        employee_2 = Employee("Christian", "Markella", 10_000)
-
-        self.assertEqual(employee_1.pay, 60_000)
-        self.assertEqual(employee_2.pay, 10_000)
+        self.assertEqual(self.employee_1.pay, 60_000)
+        self.assertEqual(self.employee_2.pay, 10_000)
         
-        employee_1.apply_raise()
-        employee_2.apply_raise()
+        self.employee_1.apply_raise()
+        self.employee_2.apply_raise()
 
-        self.assertEqual(employee_1.pay, 66_000)
-        self.assertEqual(employee_2.pay, 11_000)
+        self.assertEqual(self.employee_1.pay, 66_000)
+        self.assertEqual(self.employee_2.pay, 11_000)
 
 if __name__ == "__main__":
     unittest.main()
