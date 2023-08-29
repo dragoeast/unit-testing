deeed07a8bfe4da598eb441a6d0f62cf549a9fea
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-08-29 11:04:12 -0500

    test_fullname and test_pay were added

--- test_employee.py
+++ test_employee.py
@@ -14,5 +14,29 @@ class TestEmployee(unittest.TestCase):
 
         self.assertEqual(employee_2.email, "Mark.Markella@email.com")
 
+    def test_fullname(self):
+        employee_1 = Employee("Krisztian", "Markella", 60_000)
+        employee_2 = Employee("Christian", "Markella", 10_000)
+
+        self.assertEqual(employee_1.fullname, "Krisztian Markella")
+        self.assertEqual(employee_2.fullname, "Christian Markella")
+        
+        employee_2.first = "Mark"
+
+        self.assertEqual(employee_2.fullname, "Mark Markella")
+
+    def test_raise(self):
+        employee_1 = Employee("Krisztian", "Markella", 60_000)
+        employee_2 = Employee("Christian", "Markella", 10_000)
+
+        self.assertEqual(employee_1.pay, 60_000)
+        self.assertEqual(employee_2.pay, 10_000)
+        
+        employee_1.apply_raise()
+        employee_2.apply_raise()
+
+        self.assertEqual(employee_1.pay, 66_000)
+        self.assertEqual(employee_2.pay, 11_000)
+
 if __name__ == "__main__":
     unittest.main()
